aad4b96e5eef016fe2ef980d6225d9afdb751898
DevKrazy <ndmroot-git@yahoo.fr>
2020-12-16 19:18:35 +0100

    Code cleanup

--- sound/sound_player.py
+++ sound/sound_player.py
@@ -4,18 +4,11 @@ MAX_CHANNEL_NUMBER = 30
 
 
 def play_sound(sound, profile, channel_number=0, extension="wav"):
-    if pygame.mixer.Channel(channel_number).get_busy():
-        if channel_number <= MAX_CHANNEL_NUMBER:
-            play_sound(sound, profile, channel_number + 1)
-
-            print("Maximum channel number reached (" + str(MAX_CHANNEL_NUMBER) + ")")
+    if pygame.mixer.Channel(channel_number).get_busy():  # if the channel is busy
+        if channel_number <= MAX_CHANNEL_NUMBER:  # if we have free channels left
+            play_sound(sound, profile, channel_number + 1)  # we recursively calls the play_sound on the next channel
+        else:
+            print("Maximum channel number reached (" + str(MAX_CHANNEL_NUMBER) + ")")  # displays a warning
             # TODO: print warning on LCD
-    else:
+    else:  # channel is free we play the sound
         pygame.mixer.Channel(channel_number).play(profile.get_path + sound.get_sound_filename() + "." + extension)
-
-
-# def playsound(sound, channel_number=0):
-#     if pygame.mixer.Channel(channel_number).get_busy() and channel_number < 29:
-#         playsound(sound, channel_number + 1)
-#     else:
-#         pygame.mixer.Channel(channel_number).play(sound)
