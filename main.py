d925ec3496525c9749e8093db5d107b970b1a73e
DevKrazy <ndmroot-git@yahoo.fr>
2020-12-17 23:19:52 +0100

    main.py rewrote

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-import LCD
-import sound.sound_profile as sprofile
-import sound.sound_player as splay
-from sound.sounds import Sound
+from sound import sound_player, sound_profile, sounds
+from pyjoycon import device
+from pyjoycon.joycon import JoyCon
 from time import sleep
 
-LCD.init_screen()
-LCD.set_color(0x80, 0x80, 0x80)
-LCD.display_profile_menu(sprofile.get_name(0))
+joycon_R = JoyCon(*device.get_ids("R"))
+joycon_L = JoyCon(*device.get_ids("L"))
 
-# events loop
-#while True:
+# current profile's id
+profile_id = 0
 
-   # sleep(0.015) # joycon refresh rate
+# right joycon buttons
+a = 0
+b = 0
+x = 0
+y = 0
+# left joycon buttons
+up = 0
+down = 0
+left = 0
+right = 0
+
+while True:
+
+    # buttons update
+    # TODO: use a dict or an array to store buttons
+    # TODO: make a function to detect which button was pressed
+    # TODO: play a sound according to this function's return value (tuple?)
+    a_current = joycon_R.get_button_a()
+    b_current = joycon_R.get_button_b()
+    x_current = joycon_R.get_button_x()
+    y_current = joycon_R.get_button_y()
+    up_current = joycon_L.get_button_up()
+    down_current = joycon_L.get_button_down()
+    left_current = joycon_L.get_button_left()
+    right_current = joycon_L.get_button_right()
+
+    if a != a_current:
+        sound_player.play_sound(sounds.SOUND_A, profile_id)
+
+
+    sleep(0.015) # joycon refresh rate
 
 
 
--- sound/sound_player.py
+++ sound/sound_player.py
@@ -1,14 +1,16 @@
 import pygame.mixer
+from sound import sound_profile
 
 MAX_CHANNEL_NUMBER = 30
 
 
-def play_sound(sound, profile, channel_number=0, extension="wav"):
+def play_sound(sound, profile_id, channel_number=0, extension="wav"):
     if pygame.mixer.Channel(channel_number).get_busy():  # if the channel is busy
         if channel_number <= MAX_CHANNEL_NUMBER:  # if we have free channels left
-            play_sound(sound, profile, channel_number + 1)  # we recursively calls the play_sound on the next channel
+            play_sound(sound, profile_id, channel_number + 1)  # we recursively calls the play_sound on the next channel
         else:
             print("Maximum channel number reached (" + str(MAX_CHANNEL_NUMBER) + ")")  # displays a warning
             # TODO: print warning on LCD
     else:  # channel is free we play the sound
-        pygame.mixer.Channel(channel_number).play(profile.get_path + sound.get_sound_filename() + "." + extension)
+        print("Playing " + str(sound_profile.get_full_path() + sound.get_sound_filename() + "." + extension) + " in channel " + str(channel_number))
+        pygame.mixer.Channel(channel_number).play(sound_profile.get_full_path() + sound.get_sound_filename() + "." + extension)
