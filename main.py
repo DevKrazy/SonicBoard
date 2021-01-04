3a893b673a7246dc2b03840bdbc04fb9800a193b
Thomas <thomas.scholivet@gmail.com>
2021-01-04 21:25:22 +0100

    sonicpi implementation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from sound import sound_player, sound_profile
 from utils import joycon_utils
 from pyjoycon import JoyCon, get_R_id, get_L_id
 from time import sleep
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+sender = udp_client.SimpleUDPClient('127.0.0.1', 4560)
 
 # TEST
 id_R = get_R_id()
@@ -20,6 +24,7 @@ while True:
 
     for button in pressed_buttons:
         print(button)
+        sender.send_message('/button', '/home/thomas/PycharmProjects/SonicBoard/sound_assets/memes/' + button + '.wav')
 
     previous_buttons_state = joycon_utils.extract_useful_inputs(joycon_L, joycon_R)
 
--- sound/sound_player.py
+++ sound/sound_player.py
@@ -4,10 +4,10 @@ from sound import sound_profile
 MAX_CHANNEL_NUMBER = 30
 
 
-def play_sound(sound, profile_id, channel_number=0, extension="wav"):
+def play_soundPYGAME(sound, profile_id, channel_number=0, extension="wav"):
     if pygame.mixer.Channel(channel_number).get_busy():  # if the channel is busy
         if channel_number <= MAX_CHANNEL_NUMBER:  # if we have free channels left
-            play_sound(sound, profile_id, channel_number + 1)  # we recursively calls the play_sound on the next channel
+            play_soundPYGAME(sound, profile_id, channel_number + 1)  # we recursively calls the play_sound on the next channel
         else:
             print("Maximum channel number reached (" + str(MAX_CHANNEL_NUMBER) + ")")  # displays a warning
             # TODO: print warning on LCD
