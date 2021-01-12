f3ad10e6652f22f323e886f96984adf6075595b8
DevKrazy <ndmroot-git@yahoo.fr>
2021-01-12 14:11:59 +0100

    Added wav files for the drum profile. Removed all joycon related code.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import LCD
 from utils import defcom
-from inputs import joycon_inputs
 from sound import sound_profile
+import pygame
+
 
 from time import sleep
 
@@ -17,27 +18,19 @@ defcom.pinMode(POT, "INPUT")
 
 ########## TEST #########
 LCD.init_screen()
-LCD.set_color(0xFF, 0x00, 0x00)
+LCD.set_color(0xFF, 0x00, 0x99)
 LCD.display_main_menu()
 ######### END ###########
 
 # previous buttons states
-previous_joycon_buttons_state = joycon_inputs.get_buttons_state()
 previous_left_button_state = defcom.digitalRead(BUTTON_LEFT)
 previous_right_button_state = defcom.digitalRead(BUTTON_RIGHT)
 
 while True:
     # current buttons states
-    current_joycon_buttons_state = joycon_inputs.get_buttons_state()
     current_left_button_state = defcom.digitalRead(BUTTON_LEFT)
     current_right_button_state = defcom.digitalRead(BUTTON_RIGHT)
 
-    # joycon buttons computing
-    pressed_joycon_buttons = joycon_inputs.get_pressed_buttons(previous_joycon_buttons_state, current_joycon_buttons_state)
-
-    for button in pressed_joycon_buttons:
-        # TODO: play sound
-        print(button)
 
     # profile buttons computing
     if previous_left_button_state != current_left_button_state and current_left_button_state == 1:
@@ -50,6 +43,7 @@ while True:
         LCD.display_main_menu()
 
     # current buttons state becomes the previous state for the next loop iteration
-    previous_joycon_buttons_state = joycon_inputs.get_buttons_state()
+    previous_left_button_state = current_left_button_state
+    previous_right_button_state = current_right_button_state
 
     sleep(0.015)  # joycon refresh rate
